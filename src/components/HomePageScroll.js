59338ad0d1db25bf2044242bdd54f12a2ff33e43
deepak kumar sah <brandclever.deepak2@gmail.com>
2024-03-21 16:37:35 +0530

    add scrollbar section in home page

--- src/components/HomePageScroll.js
+++ src/components/HomePageScroll.js
@@ -1,76 +1,60 @@
-import React, { useEffect, useRef } from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
-import $ from 'jquery';
+import React, { useEffect, useRef, useState } from 'react'
+import Menu from './menucard';
+import { Col, Row } from 'react-bootstrap';
 
 export default function HomePageScroll() {
 
+    const [menuData] = useState(Menu);
+
     useEffect(() => {
-        var $window = $(window);
-        var $holder = $("#slides-holder");
-        var $slides = $holder.find(".slide");
+        const handleScroll = () => {
+            const showcaseContainer = document.querySelector('.showcase-container');
+            const showcaseElements = document.querySelectorAll('.showcase');
 
+            if (showcaseContainer && showcaseElements) {
+                const scrollPosition = showcaseContainer.scrollTop;
 
-        $slides.each(function (index, element) {
-            $(element).css("z-index", 100 + index).after($("<div />", {
-                class: "slide-fake-height"
-            }));
-        });
+                showcaseElements.forEach((element, index) => {
+                    const slideHeight = element.offsetHeight;
+                    const slideTop = element.offsetTop - showcaseContainer.offsetTop;
 
-        var $fakeHeights = $slides.next(".slide-fake-height");
-        var $toReize = $holder.add($slides).add($fakeHeights);
-        var scrollFixer = 3;
+                    if (slideTop <= scrollPosition && slideTop + slideHeight > scrollPosition) {
+                        element.classList.add('highlighted');
+                    } else {
+                        element.classList.remove('highlighted');
+                    }
+                });
+            }
+        };
 
-        $window.on("resize", onResize).resize();
-
-        $window.on("scroll", function (event) {
-            (!!window.requestAnimationFrame) ? requestAnimationFrame(onScroll) : onScroll();
-        });
-
-        function onResize(event) {
-            $toReize.css({
-                height: $window.innerHeight() + "px"
-            });
-            onScroll();
+        const showcaseContainer = document.querySelector('.showcase-container');
+        if (showcaseContainer) {
+            showcaseContainer.addEventListener('scroll', handleScroll);
         }
 
-        function onScroll(event) {
-
-            var scrollTop = Math.abs($window.scrollTop()) + scrollFixer;
-
-            $slides.each(function (index, element) {
-
-                var $slide = $(element);
-                var $fakeHeight = $slide.next(".slide-fake-height");
-
-                //This can be optimised a lot, but this is just a demo effect
-                var slideTop = $slide.hasClass("fixed") ? $fakeHeight.offset().top : $slide.offset().top;
-                if (slideTop < scrollTop) {
-                    $slide.addClass("fixed");
-                    $fakeHeight.addClass("visible");
-                } else {
-                    $slide.removeClass("fixed");
-                    $fakeHeight.removeClass("visible");
-                }
-            });
-        }
-    })
-
+        return () => {
+            if (showcaseContainer) {
+                showcaseContainer.removeEventListener('scroll', handleScroll);
+            }
+        };
+    }, []);
 
     return (
-        <div style={{ background: "gray" }} >
-            <div id="slides-holder">
-                <div class="slide" id="slide-1">
-
-                </div>
-                <div class="slide" id="slide-2">
-
-                </div>
-                <div class="slide" id="slide-3">
-
-                </div>
-
+        <div className='container_slider'>
+            <div className="showcase-container">
+                <Row>
+                    {menuData.map((curElem, index) => (
+                        <div key={index} className="showcase">
+                            <Col md={6}>
+                                <h1>hello</h1>
+                            </Col>
+                            <Col md={6}>
+                                <div className="image"><img src={curElem.image} alt="" /></div>
+                            </Col>
+                        </div>
+                    ))}
+                </Row>
             </div>
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         </div>
-    )
+    );
 }
