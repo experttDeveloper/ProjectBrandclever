3a7eea26364921896d4312b52f82a268168950c8
deepak <brandclever.deepak2@gmail.com>
2024-04-18 18:25:53 +0530

    modification in service

--- src/components/OurServices.js
+++ src/components/OurServices.js
@@ -3,72 +3,12 @@ import { Link } from 'react-router-dom';
 import '../css/OurServices.css';
 import DoneAllIcon from '@mui/icons-material/DoneAll';
 import { Col, Row } from 'react-bootstrap';
+import services from '../data/Service.json'
 
 export default class OurServices extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      services: [
-        {
-          title: 'Web Development',
-          icon: process.env.PUBLIC_URL + '/images/service/website_develop.png',
-          description: 'At Brandclever, Our professionals harness modern programming languages and frameworks to construct secure, scalable, high-performing web solutions. Prioritizing functionality, efficiency, and user-friendliness, we ensure an exceptional experience for your audience. Our expertise spans prominent platforms including PHP frameworks, WordPress, and Shopify. Entrust us to elevate your online presence, delivering bespoke web development solutions that resonate with your brand and drive tangible results. Welcome to the pinnacle of web development excellence with Brandclever.',
-          additionalInfo: '<ul><li>Shopify Store Development</li><li>Wordpress Site Development</li><li>Landing Page Website</li><li>Backend Development</li><li>Frontend Development</li></ul>',
-          url: '/web-development',
-          tools: ['Shopify Store Development', 'Wordpress Site Development', 'Landing Page Website', 'Backend Development', 'Frontend Development'],
-        },
-        {
-          title: 'Graphic Design',
-          icon: process.env.PUBLIC_URL + '/images/service/graphic_design.png',
-          description: "We at Brandclever shape concepts into impactful designs, imprinting professionalism onto your brand. Our creative team crafts visually stunning graphics that command attention and resonate with your audience, utilizing leading tools such as Adobe Photoshop, Coreldraw, Figma, and Illustrator. Let's elevate your brand's visual identity together.",
-          additionalInfo: '<ul><li>Banner Designs</li><li>Icon Designs</li><li>Mobile Application Designs</li><li>Logo Designs</li><li>Business Cards</li><li>Brochures</li><ul>',
-          url: '/graphic-degin',
-          tools: ['Banner Designs', 'Icon Designs', 'Mobile Application Designs', 'Logo Designs', 'Business Cards', 'Brochures'],
-        },
-        {
-          title: 'CMS Solution',
-          icon: process.env.PUBLIC_URL + '/images/service/cms_solution.png',
-          description: "Unlock seamless digital control with Brandclever's CMS solutions. Empowering your online presence, our Content Management System (CMS) service offers a user-friendly interface for effortless content management. Stay dynamic and authentic as you effortlessly update and expand your website's content—trust in our expertise, backed by leading tools like WordPress, Drupal, Joomla, Wix, Magento, and more.",
-          additionalInfo: '<ul><li>Content Creation and Storage</li><li>User Access Control</li><li>Workflow Automation</li><li>Multi-Platform Publishing</li><li>Content Analysis</li><li>Content Migration Services</li></ul>',
-          url: '/cms-solution',
-          tools: ['Content Creation and Storage', 'User Access Control', 'Workflow Automation', 'Multi-Platform Publishing', 'Content Analysis', 'Content Migration Services'],
-        },
-        {
-          title: 'Content Strategy',
-          icon: process.env.PUBLIC_URL + '/images/service/content_strategy.png',
-          description: 'Content Management Solutions using technologies like WordPress, Shopify, Magento 2.',
-          additionalInfo: '<ul><li>Content Creation and Storage</li><li>User Access Control</li><li>Workflow Automation</li><li>Multi-Platform Publishing</li><li>Content Analysis</li><li>Content Migration Services</li></ul>',
-          url: '/content-strategy',
-          tools: ['Content Audits', 'Content Calendar and Edit`ing', 'Content Distribution and Promotion', 'Conversion website and landing pages', 'Email & social media copy', 'Optimized blog posts and articles'],
-        },
-        {
-          title: 'Mern Stack',
-          icon: process.env.PUBLIC_URL + '/images/service/mern_stack.png',
-          description: 'Content Management Solutions using technologies like WordPress, Shopify, Magento 2.',
-          additionalInfo: '<ul><li>Content Creation and Storage</li><li>User Access Control</li><li>Workflow Automation</li><li>Multi-Platform Publishing</li><li>Content Analysis</li><li>Content Migration Services</li></ul>',
-          url: '/mern-stack',
-          tools: ['Shopify Store Development', 'Wordpress Site Development', 'Landing Page Website', 'Backend Development', 'Frontend Development'],
-        },
-        {
-          title: 'Software Testing',
-          icon: process.env.PUBLIC_URL + '/images/service/software_testing.png',
-          description: 'Content Management Solutions using technologies like WordPress, Shopify, Magento 2.',
-          additionalInfo: '<ul><li>Content Creation and Storage</li><li>User Access Control</li><li>Workflow Automation</li><li>Multi-Platform Publishing</li><li>Content Analysis</li><li>Content Migration Services</li></ul>',
-          url: '/software-testing',
-          tools: ['Email Templates', 'Landing Pages', 'UI Designs', 'Creative Web Layouts', 'Mobile Responsive Websites'],
-        },
-        {
-          title: 'Framework',
-          icon: process.env.PUBLIC_URL + '/images/service/framework.png',
-          description: 'Content Management Solutions using technologies like WordPress, Shopify, Magento 2.',
-          additionalInfo: '<ul><li>Content Creation and Storage</li><li>User Access Control</li><li>Workflow Automation</li><li>Multi-Platform Publishing</li><li>Content Analysis</li><li>Content Migration Services</li></ul>',
-          url: '/framework',
-          tools: ['PHP - CMS Development', 'Custom PHP Framework Development', 'PHP Framework API Integration', 'PHP Framework Plugin Development', 'Performance optimization', 'PHP Framework Migration'],
-
-        },
-      ],
-    };
   }
 
   render() {
@@ -81,7 +21,7 @@ export default class OurServices extends Component {
 
             <Row>
               {
-                this.state.services.slice(0, 3).map((service, key) => {
+                services.data.slice(0, 3).map((service, key) => {
                   return (
                     <Col md={4}>
                       <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
@@ -120,121 +60,9 @@ export default class OurServices extends Component {
                   )
                 })
               }
-              {/* <Col md={4}>
-                <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
-                  <div class="flipper">
-                    <div class="front">
-                      <div className='logo_img'>
-                        <img className='srv_bg_img' src={`/images/service/bg_srv.png`} />
-                        <img className='srv_bg_web' src={`/images/service/web-dvl.svg`} />
-                        <h3>Web Development</h3>
-                        <p>At Brandclever, Our professionals will Utilise modern programming languages and frameworks to build secure, scalable, and high-performing web solutions. We prioritize functionality, efficiency, and user-friendliness, guaranteeing the best experience for your audience. Our Web Development Platforms PHP frameworks        WordPress Shopify...</p>
-                      </div>
-                    </div>
-                    <div class="back">
-                      <div className='back_logo_img'>
-                        <img className='srv_bg_img' src={`/images/service/bg_srv.png`} />
-                        <img className='srv_bg_web' src={`/images/service/web-dvl.svg`} />
-                        <h3>Web Development</h3>
-                      </div>
-                      <div class="oposite" bis_skin_checked="1">
-                        <ul>
-                          <li><span><DoneAllIcon /></span> Shopify Store Development</li>
-                          <li><span><DoneAllIcon /></span> Wordpress Site Development</li>
-                          <li><span><DoneAllIcon /></span> Landing Page Website</li>
-                          <li><span><DoneAllIcon /></span> Backend Development</li>
-                          <li><span><DoneAllIcon /></span> Frontend Development</li>
-                        </ul>
-                      </div>
-                      <div className='read_more'>
-                        <a href='#'>Read More</a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </Col>
-              <Col md={4}>
-                <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
-                  <div class="flipper">
-                    <div class="front">
-                      <div className='logo_img'>
-                        <img className='srv_bg_img' src={`/images/service/bg_srv.png`} />
-                        <img className='srv_bg_web' src={`/images/service/web-dvl.svg`} />
-                        <h3>Digital Marketing</h3>
-                        <p>Be the first one to be seen on search engines. We have immense expertise in SEO and
-                          marketing to draw new traffic. We're adaptive, serving various industries with different and effective strategies for businesses of any scale or sector. Our Digital Marketing Tools Google Analytics Semrush Google Ads Active Campaign Ahrefs...</p>
-                      </div>
-                    </div>
-                    <div class="back">
-                      <div className='back_logo_img'>
-                        <img className='srv_bg_img' src={`/images/service/bg_srv.png`} />
-                        <img className='srv_bg_web' src={`/images/service/web-dvl.svg`} />
-                        <h3>Digital Marketing</h3>
-                      </div>
-                      <div class="oposite" bis_skin_checked="1">
-                        <ul>
-                          <li><span><DoneAllIcon /></span> Search Engine Optimization(SEO)</li>
-                          <li><span><DoneAllIcon /></span> Social Media Marketing</li>
-                          <li><span><DoneAllIcon /></span> Search Engine Marketing(SEM)</li>
-                          <li><span><DoneAllIcon /></span> Analytics Consultation</li>
-                          <li><span><DoneAllIcon /></span> Inbound Marketing</li>
-                        </ul>
-                      </div>
-                      <div className='read_more'>
-                        <a href='#'>Read More</a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </Col>
-              <Col md={4}>
-                <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
-                  <div class="flipper">
-                    <div class="front">
-                      <div className='logo_img'>
-                        <img className='srv_bg_img' src={`/images/service/bg_srv.png`} />
-                        <img className='srv_bg_web' src={`/images/service/web-dvl.svg`} />
-                        <h3>Graphic Design</h3>
-                        <p>We transform ideas into creative designs, leaving a professional mark on your brand.
-                          Our creative team provides quality and attention-grabbing pixels and stop-scrolling graphics. Let's create graphics that speak and connect your brand with your audience. Our Graphic design Tools Adobe Photoshop Coreldraw Figma Illustrator Ado...</p>
-                      </div>
-                    </div>
-                    <div class="back">
-                      <div className='back_logo_img'>
-                        <img className='srv_bg_img' src={`/images/service/bg_srv.png`} />
-                        <img className='srv_bg_web' src={`/images/service/web-dvl.svg`} />
-                        <h3>Graphic Design</h3>
-                      </div>
-                      <div class="oposite" bis_skin_checked="1">
-                        <ul>
-                          <li><span><DoneAllIcon /></span> Logo Designst</li>
-                          <li><span><DoneAllIcon /></span> Banner Designs</li>
-                          <li><span><DoneAllIcon /></span> Mobile Application Designs</li>
-                          <li><span><DoneAllIcon /></span> Brochures</li>
-                          <li><span><DoneAllIcon /></span> Icon Designs</li>
-                        </ul>
-                      </div>
-                      <div className='read_more'>
-                        <a href='#'>Read More</a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </Col> */}
+             
             </Row>
           </div>
-
-          {/* <div className="service-row">
-              {this.state.services.map((service, index) => (
-                <div className='col serviceCount'>
-                <div key={index} className="frontCont">
-                  <img src={service.icon} alt={service.title} />
-                  <h3>{service.title}</h3>                 
-
-                </div>
-                </div>
-              ))} 
-            </div> */}
           <div className="read-btnn">
             <Link to="/service" className='button_slide slide_down'>Read More</Link>
           </div>
--- src/pages/UpdatedService.jsx
+++ src/pages/UpdatedService.jsx
@@ -1,70 +1,11 @@
 import React from 'react'
 import '../css/updateservice.css'
 import JoinTeam from '../components/JoinTeam'
-import { Col, Row } from 'react-bootstrap'
 import TopHeader from '../components/TopHeader'
 import Portfolio from '../components/Portfolio';
 import { Link } from 'react-router-dom'
+import services from '../data/Service.json'
 
-const services = [
-    {
-        title: 'Web Development',
-        icon: process.env.PUBLIC_URL + '/images/service/website_develop.png',
-        description: 'At Brandclever, Our professionals harness modern programming languages and frameworks to construct secure, scalable, high-performing web solutions. Prioritizing functionality, efficiency, and user-friendliness, we ensure an exceptional experience for your audience. Our expertise spans prominent platforms including PHP frameworks, WordPress, and Shopify. Entrust us to elevate your online presence, delivering bespoke web development solutions that resonate with your brand and drive tangible results. Welcome to the pinnacle of web development excellence with Brandclever.',
-        additionalInfo: '<ul><li>Shopify Store Development</li><li>Wordpress Site Development</li><li>Landing Page Website</li><li>Backend Development</li><li>Frontend Development</li></ul>',
-        url: '/web-development',
-        tools: ['Shopify Store Development', 'Wordpress Site Development', 'Landing Page Website', 'Backend Development', 'Frontend Development'],
-    },
-    {
-        title: 'Graphic Design',
-        icon: process.env.PUBLIC_URL + '/images/service/graphic_design.png',
-        description: "We at Brandclever shape concepts into impactful designs, imprinting professionalism onto your brand. Our creative team crafts visually stunning graphics that command attention and resonate with your audience, utilizing leading tools such as Adobe Photoshop, Coreldraw, Figma, and Illustrator. Let's elevate your brand's visual identity together.",
-        additionalInfo: '<ul><li>Banner Designs</li><li>Icon Designs</li><li>Mobile Application Designs</li><li>Logo Designs</li><li>Business Cards</li><li>Brochures</li><ul>',
-        url: '/graphic-degin',
-        tools: ['Banner Designs', 'Icon Designs', 'Mobile Application Designs', 'Logo Designs', 'Business Cards', 'Brochures'],
-    },
-    {
-        title: 'CMS Solution',
-        icon: process.env.PUBLIC_URL + '/images/service/cms_solution.png',
-        description: "Unlock seamless digital control with Brandclever's CMS solutions. Empowering your online presence, our Content Management System (CMS) service offers a user-friendly interface for effortless content management. Stay dynamic and authentic as you effortlessly update and expand your website's content—trust in our expertise, backed by leading tools like WordPress, Drupal, Joomla, Wix, Magento, and more.",
-        additionalInfo: '<ul><li>Content Creation and Storage</li><li>User Access Control</li><li>Workflow Automation</li><li>Multi-Platform Publishing</li><li>Content Analysis</li><li>Content Migration Services</li></ul>',
-        url: '/cms-solution',
-        tools: ['Content Creation and Storage', 'User Access Control', 'Workflow Automation', 'Multi-Platform Publishing', 'Content Analysis', 'Content Migration Services'],
-    },
-    {
-        title: 'Content Strategy',
-        icon: process.env.PUBLIC_URL + '/images/service/content_strategy.png',
-        description: 'Content Management Solutions using technologies like WordPress, Shopify, Magento 2.',
-        additionalInfo: '<ul><li>Content Creation and Storage</li><li>User Access Control</li><li>Workflow Automation</li><li>Multi-Platform Publishing</li><li>Content Analysis</li><li>Content Migration Services</li></ul>',
-        url: '/content-strategy',
-        tools: ['Content Audits', 'Content Calendar and Edit`ing', 'Content Distribution and Promotion', 'Conversion website and landing pages', 'Email & social media copy', 'Optimized blog posts and articles'],
-    },
-    {
-        title: 'Mern Stack',
-        icon: process.env.PUBLIC_URL + '/images/service/mern_stack.png',
-        description: 'Content Management Solutions using technologies like WordPress, Shopify, Magento 2.',
-        additionalInfo: '<ul><li>Content Creation and Storage</li><li>User Access Control</li><li>Workflow Automation</li><li>Multi-Platform Publishing</li><li>Content Analysis</li><li>Content Migration Services</li></ul>',
-        url: '/mern-stack',
-        tools: ['Shopify Store Development', 'Wordpress Site Development', 'Landing Page Website', 'Backend Development', 'Frontend Development'],
-    },
-    {
-        title: 'Software Testing',
-        icon: process.env.PUBLIC_URL + '/images/service/software_testing.png',
-        description: 'Content Management Solutions using technologies like WordPress, Shopify, Magento 2.',
-        additionalInfo: '<ul><li>Content Creation and Storage</li><li>User Access Control</li><li>Workflow Automation</li><li>Multi-Platform Publishing</li><li>Content Analysis</li><li>Content Migration Services</li></ul>',
-        url: '/software-testing',
-        tools: ['Email Templates', 'Landing Pages', 'UI Designs', 'Creative Web Layouts', 'Mobile Responsive Websites'],
-    },
-    {
-        title: 'Framework',
-        icon: process.env.PUBLIC_URL + '/images/service/framework.png',
-        description: 'Content Management Solutions using technologies like WordPress, Shopify, Magento 2.',
-        additionalInfo: '<ul><li>Content Creation and Storage</li><li>User Access Control</li><li>Workflow Automation</li><li>Multi-Platform Publishing</li><li>Content Analysis</li><li>Content Migration Services</li></ul>',
-        url: '/framework',
-        tools: ['PHP - CMS Development', 'Custom PHP Framework Development', 'PHP Framework API Integration', 'PHP Framework Plugin Development', 'Performance optimization', 'PHP Framework Migration'],
-
-    },
-]
 
 export default function UpdatedService() {
 
@@ -82,7 +23,7 @@ export default function UpdatedService() {
                         <p class="fs-20 mb-0">We stay on top of our industry by being experts in yours.</p>
                     </div>
                     <div class="hex_1bx">
-                        {services.slice(0, 3).map((service, key) => {
+                        {services.data.slice(0, 3).map((service, key) => {
                             return (
                                 <div class="ctm_hex_dv1" id="borer_bx_1">
                                     <div class="ctm_hex_dv2">
@@ -98,7 +39,7 @@ export default function UpdatedService() {
                         })}
                     </div>
                     <div class="hex_2bx">
-                        {services.slice(3, 8).map((service, key) => {
+                        {services.data.slice(3, 8).map((service, key) => {
                             return (
                                 <div class="ctm_hex_dv1" id="borer_bx_4">
                                     <div class="ctm_hex_dv2">
